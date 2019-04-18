b1257870c0e5a9b355c1dd6ac99fa1bf7eb5f388
josepplloo <josepplloo@gmail.com>
2019-04-18 00:29:54 -0500

    The firts test for dealing with threads

---
+++ src/main/java/com/mycompany/call/Call.java
@@ -0,0 +1,27 @@
+package com.mycompany.call;
+
+public class Call {
+  public int number;
+  public int time;
+
+  public Call (int number,int time) {
+    this.number = number;
+    this.time = time;
+  }
+
+  public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public Integer getTime() {
+		return time;
+	}
+	public void setTime(Integer time) {
+		this.time = time;
+	}
+	public void print(){
+               System.out.println("Number:"+number + ", Time:"+time);
+        }
+}
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,7 +1,13 @@
 package com.mycompany.app;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import com.mycompany.call.Call;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
@@ -13,8 +19,24 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswertheCalls() {
+      final List<Call> calls =  Arrays.asList(new Call(1, 5), new Call(2, 6));
+      ExecutorService service =  Executors.newFixedThreadPool(2);
+
+      Runnable r = () -> {
+          System.out.println(Thread.currentThread().getName());
+          Consumer<Call> consumer = (Call call) -> {
+            System.out.println(call.getNumber());
+            try {
+              Thread.currentThread().sleep(10);
+            } catch (InterruptedException e) {
+              //TODO: handle exception
+            }
+          };       
+          calls.forEach(consumer);
+       
+      };
+    service.execute(r);
+    assertTrue( true );
     }
 }
